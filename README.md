1237bc91c3d4ef4dda2468dcba5aa6c94ccf35f9
Marcus Fedarko <mfedarko@umd.edu>
2022-01-31 03:58:29 -0800

    repo skeleton work

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # strainFlye
-Pipeline for calling and phasing rare mutations in metagenome-assembled genomes
+
+strainFlye is a pipeline for calling, analyzing, and phasing rare mutations
+in metagenome-assembled genomes produced from HiFi sequencing data.
+
+![strainFlye pipeline diagram](https://github.com/fedarko/strainFlye/raw/main/docs/strainflye-pipeline.png)
+
+This repository is under active development; we're working on porting our code
+from ad hoc analyses to an easier-to-use pipeline. In the meantime, if you have
+any questions, please feel free to open an issue.
---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# This file adapted from https://github.com/biocore/qurro/blob/master/setup.py
+
+from setuptools import find_packages, setup
+from strainflye import __version__
+
+classifier_str = """
+    Development Status :: 3 - Alpha
+    License :: OSI Approved :: MIT License
+    Topic :: Scientific/Engineering
+    Topic :: Scientific/Engineering :: Bio-Informatics
+    Programming Language :: Python :: 3 :: Only
+    Operating System :: Unix
+    Operating System :: POSIX
+    Operating System :: MacOS :: MacOS X
+"""
+classifiers = [s.strip() for s in classifier_str.split("\n") if s]
+
+description = (
+    "Pipeline for analyzing rare mutations in metagenome-assembled genomes"
+)
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup_reqs = []
+with open("setup_requirements.txt", "r") as f:
+    for line in f:
+        if not line.startswith("#"):
+            setup_reqs.append(line.strip())
+
+setup(
+    name="strainFlye",
+    version=__version__,
+    license="BSD",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Marcus Fedarko",
+    author_email="mfedarko@ucsd.edu",
+    maintainer="Marcus Fedarko",
+    maintainer_email="mfedarko@ucsd.edu",
+    url="https://github.com/fedarko/strainFlye",
+    packages=find_packages(),
+    install_requires=[
+        # pysamstats requires pysam < 0.16 as of writing
+        "pysam < 0.16",
+        "pysamstats",
+        "scikit-bio",
+        "click",
+    ],
+    setup_requires=setup_reqs,
+    # Based on how Altair splits up its requirements:
+    # https://github.com/altair-viz/altair/blob/master/setup.py
+    extras_require={
+        "dev": [
+            "pytest >= 4.2",
+            "pytest-cov >= 2.0",
+            "flake8",
+            "black",
+        ]
+    },
+    classifiers=classifiers,
+    entry_points={
+        "console_scripts": ["strainFlye=strainflye._cli:run"],
+    },
+    python_requires=">=3.6",
+)
