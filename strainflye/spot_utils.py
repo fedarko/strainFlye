894bc0b6de37a7cd52de67ce4482e0cc0d0af2c6
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2022-06-17 18:48:10 -0700

    ENH: fleshing out the hotspot feature stuff
    WIP, but i think the next steps are feasible

--- strainflye/spot_utils.py
+++ strainflye/spot_utils.py
@@ -1,6 +1,11 @@
 # Utilities for strainFlye spot.
 
 
+import skbio
+from strainflye import bcf_utils
+from strainflye.errors import ParameterError
+
+
 def find_hotspot_features(
     bcf,
     features,
@@ -8,4 +13,35 @@ def find_hotspot_features(
     min_perc_mutations,
     output_hotspot_features,
 ):
-    pass
+    # Load BCF
+    bcf_obj, thresh_type, thresh_min = bcf_utils.parse_bcf(bcf)
+    bcf_contigs = set(bcf_obj.header.contigs)
+
+    # Load features. skbio.io.read() with GFF3 files defaults to a generator of
+    # tuples, where the first entry is the sequence ID (e.g. "edge_1") and the
+    # second entry is a skbio.metadata.IntervalMetadata object describing this
+    # feature.
+    contig_and_im_tuples = skbio.io.read(features, format="gff3")
+    for contig, im in contig_and_im_tuples:
+        if contig not in bcf_contigs:
+            raise ParameterError(
+                "The features file describes feature(s) located on contig "
+                f"{contig}, but this contig is not described in the BCF file."
+            )
+
+        # TODO: record all mutated positions in this contig
+
+        # HACK: iterate through all features that belong to this contig.
+        # See https://github.com/biocore/scikit-bio/issues/1817 regarding why
+        # we can't just say im.query(), at least at the moment.
+        for feature in im.query(metadata={}):
+            if len(feature.bounds) < 1:
+                raise ParameterError(
+                    "A feature exists that doesn't have at least one set of "
+                    f"bounds: {feature}"
+                )
+
+            # TODO: assert that bounds don't overlap with each other, right?
+            # or just compute a set of positions correspodning to all positions
+            # in this feature, then "&" that with the set of mutated positions.
+            # could be sped up but probs good enough for now
