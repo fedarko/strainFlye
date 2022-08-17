fd45b6af922ba5959ee15b6116ea36eea06ec1f5
Marcus Fedarko <mfedarko@ucsd.edu>
2022-08-16 23:16:05 -0700

    TST: FASTA vs. GFA length tests for align

--- strainflye/tests/test_align_utils.py
+++ strainflye/tests/test_align_utils.py
@@ -7,6 +7,7 @@ from strainflye.errors import SequencingDataError, GraphParsingError
 TI_DIR = os.path.join("strainflye", "tests", "inputs")
 S1 = os.path.join(TI_DIR, "sample1.gfa")
 
+
 def test_get_coords_good():
     samfile = pysam.AlignmentFile(os.path.join(TI_DIR, "camp-sf.sam"), "r")
     for aln in samfile.fetch():
@@ -15,8 +16,7 @@ def test_get_coords_good():
 
 def test_check_contigs_in_graph_good():
     au.check_contigs_in_graph(
-        {"1": 8, "2": 10, "3": 21, "4": 7, "5": 8, "6": 4},
-        S1
+        {"1": 8, "2": 10, "3": 21, "4": 7, "5": 8, "6": 4}, S1
     )
 
 
@@ -32,7 +32,7 @@ def test_check_contigs_in_graph_missing():
 
     for n2l in bad_inputs:
         with pytest.raises(SequencingDataError) as ei:
-            au.check_contigs_in_graph( n2l, S1)
+            au.check_contigs_in_graph(n2l, S1)
         assert str(ei.value) == (
             "Segment names in the GFA file don't match contig names in the "
             "FASTA file."
@@ -46,3 +46,23 @@ def test_check_contigs_in_graph_empty_gfa():
     with pytest.raises(GraphParsingError) as ei:
         au.check_contigs_in_graph({"1": 8}, gf)
     assert str(ei.value) == f"Less than 1 segment(s) are given in {gf}."
+
+
+def test_check_contigs_in_graph_diff_lengths():
+    with pytest.raises(SequencingDataError) as ei:
+        au.check_contigs_in_graph(
+            {"1": 9, "2": 10, "3": 21, "4": 7, "5": 8, "6": 4}, S1
+        )
+    assert str(ei.value) == (
+        "Contig 1 has length 9 in the FASTA file, but length 8 in the GFA "
+        "file."
+    )
+
+    with pytest.raises(SequencingDataError) as ei:
+        au.check_contigs_in_graph(
+            {"1": 8, "2": 10, "3": 21, "4": 7, "5": 8, "6": 4000}, S1
+        )
+    assert str(ei.value) == (
+        "Contig 6 has length 4,000 in the FASTA file, but length 4 in the GFA "
+        "file."
+    )
