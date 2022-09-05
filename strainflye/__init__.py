06e624a77b61d73ae3ba2a8a83dfdd9adcafeb14
Marcus Fedarko <mfedarko@ucsd.edu>
2022-09-04 22:28:35 -0700

    MNT: nicer __init__ experience

--- strainflye/__init__.py
+++ strainflye/__init__.py
@@ -1,1 +1,18 @@
+"""
+strainFlye is a pipeline for analyzing rare mutations in metagenomes.
+
+(It's mainly intended to be used as a command-line tool, but I guess it could
+be useful to import it as a Python module if you'd like to use some of its
+utility functions.)
+"""
+
+import os
+
+
 __version__ = "0.0.1"
+
+# Kind of a hack -- make it so that all python files in this directory, except
+# those starting with _, can be imported when we say "from strainflye import *"
+# See https://stackoverflow.com/a/1057534
+neighbors = os.listdir(os.path.dirname(__file__))
+__all__ = [m[:-3] for m in neighbors if m.endswith(".py") and not m.startswith("_")]
