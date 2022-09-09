efa32b4f926404130512bc402f04c2be0569c842
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2022-09-09 01:48:14 -0700

    MNT: bump up pysam max depth to 100 million

--- strainflye/config.py
+++ strainflye/config.py
@@ -1,6 +1,6 @@
 # Max depth at any position in the alignment: needed for pysam, see the
 # pysamstats docs -- https://github.com/alimanfoo/pysamstats
-MAX_DEPTH_PYSAM = 10000000
+MAX_DEPTH_PYSAM = 100000000
 
 # Prefix of diversity index columns in diversity index TSV files
 DI_PREF = "DivIdx"
