2f65cb6cd9d2ca9098a1c755e29a5afd5eebfbb1
Marcus Fedarko <mfedarko@ucsd.edu>
2022-10-14 22:04:13 -0700

    TST: log_bin_ct_info in covskew

--- strainflye/tests/test_dynam_utils.py
+++ strainflye/tests/test_dynam_utils.py
@@ -1,5 +1,6 @@
 from pytest import approx
 from strainflye import dynam_utils as du
+from strainflye.tests.utils_for_testing import mock_log
 
 
 def test_skew():
@@ -37,3 +38,27 @@ def test_update_cumulative_binned_skews():
 
     du.update_cumulative_binned_skews(c, -0.8)
     check_lists_approx_equal(c, [1, 0, 0, 0.5, -0.3])
+
+
+def test_log_bin_ct_info(capsys):
+    du.log_bin_ct_info("c1", 23, 5, mock_log)
+    assert capsys.readouterr().out == (
+        "MockLog: Creating 4 bins of length 5 bp and 1 smaller bin of length "
+        "3 bp for contig c1...\n"
+    )
+
+    du.log_bin_ct_info("c1", 23, 500, mock_log)
+    assert capsys.readouterr().out == (
+        "MockLog: Creating 1 smaller bin of length 23 bp for contig c1...\n"
+    )
+
+    du.log_bin_ct_info("c1", 500, 10, mock_log)
+    assert capsys.readouterr().out == (
+        "MockLog: Creating 50 bins of length 10 bp for contig c1...\n"
+    )
+
+    # of course we make it say "bin" instead of "bins"
+    du.log_bin_ct_info("c1", 1234, 1234, mock_log)
+    assert capsys.readouterr().out == (
+        "MockLog: Creating 1 bin of length 1,234 bp for contig c1...\n"
+    )
