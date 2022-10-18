9fec2c30b054c29aa724b27bc8c9a8b17338f47f
Marcus Fedarko <mfedarko@ucsd.edu>
2022-10-18 16:55:42 -0700

    TST: initial CDS feature validation stuff

--- strainflye/tests/test_gff_utils.py
+++ strainflye/tests/test_gff_utils.py
@@ -1,7 +1,14 @@
+# Note that a fair amount of stuff in gff_utils is tested indirectly through
+# test_spot_utils, so these tests aren't as woefully inadequate as they might
+# seem at first. (The reason for this is that gff_utils' code was abstracted
+# from code that was initially unique to spot_utils.)
+
+
 import skbio
 import pytest
 import strainflye.gff_utils as gu
 from io import StringIO as sio
+from strainflye import config
 from strainflye.errors import ParameterError
 from strainflye.tests.utils_for_testing import mock_log
 
@@ -45,3 +52,29 @@ def test_validate_basic_multi_bound_feature():
         "A feature in the GFF3 file on contig c1 exists without exactly "
         f"one set of bounds: {str(silly_feature)}"
     )
+
+
+def test_validate_if_cds_good():
+    gff = "##gff-version 3\nc1	marcus	cds	5	19	.	+	0	ID=hi"
+    cim_tuples = skbio.io.read(sio(gff), format="gff3")
+    for contig, im in cim_tuples:
+        feature = next(im.query(metadata={}))
+        is_cds, strand = gu.validate_if_cds(feature, "c1", mock_log)
+        assert is_cds
+        assert strand == "+"
+        break
+
+
+def test_validate_if_cds_noncds(capsys):
+    gff = "##gff-version 3\nc1	marcus	gene	5	19	.	+	0	ID=hi"
+    cim_tuples = skbio.io.read(sio(gff), format="gff3")
+    for contig, im in cim_tuples:
+        feature = next(im.query(metadata={}))
+        is_cds, strand = gu.validate_if_cds(feature, "c1", mock_log)
+        assert not is_cds
+        assert strand is None
+        assert capsys.readouterr().out == (
+            "MockLog: Feature hi on contig c1 has a type that is not in "
+            f"{config.CDS_TYPES}; ignoring it.\n"
+        )
+        break
