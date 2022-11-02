3d8e17d71259d28ac7d25a779f49ab219dc2ed6e
Marcus Fedarko <mfedarko@eng.ucsd.edu>
2022-11-02 15:50:14 -0700

    REL: bump version to 0.2.0

--- strainflye/__init__.py
+++ strainflye/__init__.py
@@ -9,7 +9,7 @@ utility functions.)
 import os
 
 
-__version__ = "0.1.0-dev"
+__version__ = "0.2.0"
 
 # Kind of a hack -- make it so that all python files in this directory, except
 # those starting with _, can be imported when we say "from strainflye import *"
